f8bf3bf958d494142d3f242e6c6e14d8455f22f9
unknown <alejandroff91@gmail.com>
2019-09-01 02:15:47 -0500

    Updates

---
+++ precache-manifest.549b623cf662bb16f3f809e6d8da7e80.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83aa9b010dba049b956341cf1b692421",
+    "url": "/kaptapage/index.html"
+  },
+  {
+    "revision": "bd56cda3aeb37c153fce",
+    "url": "/kaptapage/static/css/2.aa177848.chunk.css"
+  },
+  {
+    "revision": "22329196411b8c2a6d9d",
+    "url": "/kaptapage/static/css/main.987ff6e0.chunk.css"
+  },
+  {
+    "revision": "bd56cda3aeb37c153fce",
+    "url": "/kaptapage/static/js/2.57d22d63.chunk.js"
+  },
+  {
+    "revision": "22329196411b8c2a6d9d",
+    "url": "/kaptapage/static/js/main.beea82af.chunk.js"
+  },
+  {
+    "revision": "76657bc474150a08c098",
+    "url": "/kaptapage/static/js/runtime~main.8ea1afa6.js"
+  },
+  {
+    "revision": "324845f7607a04191516a23fe204293b",
+    "url": "/kaptapage/static/media/0-mobile.324845f7.png"
+  },
+  {
+    "revision": "4688728c741d425e166e22f84f722860",
+    "url": "/kaptapage/static/media/0.4688728c.png"
+  },
+  {
+    "revision": "fd999373bb770045f0a0ed03f2bcd178",
+    "url": "/kaptapage/static/media/1-mobile.fd999373.png"
+  },
+  {
+    "revision": "a48c915f3705f5be796738b29d12658d",
+    "url": "/kaptapage/static/media/1.a48c915f.png"
+  },
+  {
+    "revision": "4dcfd7c510499be26a4f3f1b9f15eea8",
+    "url": "/kaptapage/static/media/2-mobile.4dcfd7c5.png"
+  },
+  {
+    "revision": "07bc6a889240460985bff622a0f414c1",
+    "url": "/kaptapage/static/media/2.07bc6a88.png"
+  },
+  {
+    "revision": "24ee432777eaa21e3fc53d6bc38ae18f",
+    "url": "/kaptapage/static/media/3-mobile.24ee4327.png"
+  },
+  {
+    "revision": "3e07e0d5678ed3a883f850bbaf994ce0",
+    "url": "/kaptapage/static/media/3.3e07e0d5.png"
+  },
+  {
+    "revision": "a377ae4e85056e56bbdedc7625974d94",
+    "url": "/kaptapage/static/media/4-mobile.a377ae4e.png"
+  },
+  {
+    "revision": "7563c4d0675587dc622ca4d1e6e5eb6a",
+    "url": "/kaptapage/static/media/4.7563c4d0.png"
+  },
+  {
+    "revision": "29134dfdd50f272c47828ec869123210",
+    "url": "/kaptapage/static/media/ALFREDO.29134dfd.png"
+  },
+  {
+    "revision": "28be8464515cc3d1bdf3c7767881636d",
+    "url": "/kaptapage/static/media/ARITOS.28be8464.png"
+  },
+  {
+    "revision": "bb3527468fb6b354efd65c63619e4fe6",
+    "url": "/kaptapage/static/media/Brazo1.bb352746.png"
+  },
+  {
+    "revision": "ae4af28fea6381ba1831a1b1494578cb",
+    "url": "/kaptapage/static/media/Brazo3.ae4af28f.png"
+  },
+  {
+    "revision": "02cd0e8b0e399e4cc0ca8517a3406b59",
+    "url": "/kaptapage/static/media/Brazo5.02cd0e8b.png"
+  },
+  {
+    "revision": "9df303ac11b3f31a11e023a85a708174",
+    "url": "/kaptapage/static/media/Contact.9df303ac.png"
+  },
+  {
+    "revision": "db730f5827e4622ee652779decf9a6b3",
+    "url": "/kaptapage/static/media/Fondo01.db730f58.png"
+  },
+  {
+    "revision": "68333a24fd0b5333d222a0bcc974f94a",
+    "url": "/kaptapage/static/media/Fondo02.68333a24.png"
+  },
+  {
+    "revision": "ff780c9e2e3788409d32b6438a4b4866",
+    "url": "/kaptapage/static/media/Fondo03.ff780c9e.png"
+  },
+  {
+    "revision": "ef44fea6038f4093760e51e6f62d84fb",
+    "url": "/kaptapage/static/media/Fondo05.ef44fea6.png"
+  },
+  {
+    "revision": "ae2387a472fcff043435a01023993467",
+    "url": "/kaptapage/static/media/Fondo06.ae2387a4.png"
+  },
+  {
+    "revision": "09218409fe243fed30127cf74e724572",
+    "url": "/kaptapage/static/media/GELCE.09218409.png"
+  },
+  {
+    "revision": "8fd1690404cc49337645867ca354b78e",
+    "url": "/kaptapage/static/media/GELIX.8fd16904.png"
+  },
+  {
+    "revision": "69190fae4ec544c9103d8e55b109eae9",
+    "url": "/kaptapage/static/media/Globo_Viñeta.69190fae.png"
+  },
+  {
+    "revision": "634288c6bc3a6f1b2d2caf74be57c316",
+    "url": "/kaptapage/static/media/Icon_Packaging.634288c6.png"
+  },
+  {
+    "revision": "b4b21a7057bdd41a800a55563bde7ed8",
+    "url": "/kaptapage/static/media/Isotipo_Kapta.b4b21a70.png"
+  },
+  {
+    "revision": "88e094cc233486feb2a6ee53394f6f88",
+    "url": "/kaptapage/static/media/KnowHow.88e094cc.png"
+  },
+  {
+    "revision": "94b13f774fea047e9126ea9028eb4d03",
+    "url": "/kaptapage/static/media/Logo_Kapta.94b13f77.png"
+  },
+  {
+    "revision": "1c6c3e0579c6f03efe8584f601e00dd0",
+    "url": "/kaptapage/static/media/NECTAR.1c6c3e05.png"
+  },
+  {
+    "revision": "03ce1e1d88f4e24d4150ef8543999a11",
+    "url": "/kaptapage/static/media/P0.03ce1e1d.png"
+  },
+  {
+    "revision": "a7965602469a69f43121a14f4488428c",
+    "url": "/kaptapage/static/media/P1.a7965602.png"
+  },
+  {
+    "revision": "62b6f3e9c7f2bd37326969216c0c4137",
+    "url": "/kaptapage/static/media/P2.62b6f3e9.png"
+  },
+  {
+    "revision": "efe9da890aef41bd243a8e5fe6b511ab",
+    "url": "/kaptapage/static/media/P3.efe9da89.png"
+  },
+  {
+    "revision": "c50f108f17b3a43e5ea218edf8c07c28",
+    "url": "/kaptapage/static/media/P4.c50f108f.png"
+  },
+  {
+    "revision": "5f52a883acc4e35bfa01867c4d415b6c",
+    "url": "/kaptapage/static/media/Personaje_Branding.5f52a883.png"
+  },
+  {
+    "revision": "9bf78f97180e7c63907db0d28c3c6d59",
+    "url": "/kaptapage/static/media/Personaje_Digital.9bf78f97.png"
+  },
+  {
+    "revision": "74563d2dfa189ebf15058e25c0983507",
+    "url": "/kaptapage/static/media/Personaje_Packaging.74563d2d.png"
+  },
+  {
+    "revision": "d411e30642ec61e0ecd9985c0938617d",
+    "url": "/kaptapage/static/media/Personaje_Publicity.d411e306.png"
+  },
+  {
+    "revision": "50eb0209576b0bb05d4437e747d64ba9",
+    "url": "/kaptapage/static/media/Personaje_Software.50eb0209.png"
+  },
+  {
+    "revision": "5f52a883acc4e35bfa01867c4d415b6c",
+    "url": "/kaptapage/static/media/Personaje_none.5f52a883.png"
+  },
+  {
+    "revision": "be71e3c2dfd9dbff895c90b4b03e7c91",
+    "url": "/kaptapage/static/media/RURUY.be71e3c2.png"
+  },
+  {
+    "revision": "a86b0cb2c1808b445f1bc8271e5bf0e4",
+    "url": "/kaptapage/static/media/UN_DETALLE.a86b0cb2.png"
+  },
+  {
+    "revision": "69ef8cb8b2f17dfa851fc145f74287cf",
+    "url": "/kaptapage/static/media/WAFERITO.69ef8cb8.png"
+  },
+  {
+    "revision": "95fdaacb7154aff31401f7e4e8d91aad",
+    "url": "/kaptapage/static/media/Work.95fdaacb.png"
+  },
+  {
+    "revision": "756664edf246c75e6dcd41d4fa9b1b90",
+    "url": "/kaptapage/static/media/bg.756664ed.png"
+  },
+  {
+    "revision": "793d4ece0dc0581a0fda1f071b2931a8",
+    "url": "/kaptapage/static/media/fondo.793d4ece.png"
+  },
+  {
+    "revision": "48a6a29cb8cc5ea413422f1cb96910df",
+    "url": "/kaptapage/static/media/logo_kapta.48a6a29c.png"
+  },
+  {
+    "revision": "0d64016a379bb64b2fea4c2f69a2227e",
+    "url": "/kaptapage/static/media/megabit.0d64016a.png"
+  },
+  {
+    "revision": "4679139f2f5a7f0f3c8561614768da6f",
+    "url": "/kaptapage/static/media/proactive2.4679139f.png"
+  },
+  {
+    "revision": "caa719b22e6913e45ce6907adf123beb",
+    "url": "/kaptapage/static/media/team-img.caa719b2.png"
+  },
+  {
+    "revision": "ef6cd0e5bc0a6183778cbac1c6aa87b6",
+    "url": "/kaptapage/static/media/tentacion.ef6cd0e5.png"
+  },
+  {
+    "revision": "7019aef9f535e788450f7be13b11aa98",
+    "url": "/kaptapage/static/media/villanatura2.7019aef9.png"
+  }
+]);
